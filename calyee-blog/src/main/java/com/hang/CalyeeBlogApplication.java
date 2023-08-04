138fff832aebea4161d7046912aef40a4157cd00
Yikoutian1 <599882460@qq.com>
2023-08-04 15:18:33 +0800

    个人博客开始制作

---
+++ calyee-blog/src/main/java/com/hang/CalyeeBlogApplication.java
@@ -0,0 +1,18 @@
+package com.hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName CalyeeBlogApplication
+ * @Description TODO
+ * @Author QiuLiHang
+ * @DATE 2023/8/4 15:14
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class CalyeeBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalyeeBlogApplication.class, args);
+    }
+}
